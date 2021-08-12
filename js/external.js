5cd19255c78983d15dd7f0063cbfd26be11ab4fc
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-08-12 15:45:46 -0500

    Feat: 1st alerts and color prompt

---
+++ js/external.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log("Hello from external JavaScript.");
+
+alert('Welcome to my Website!');
+
+var userColor = prompt('What is your favorite color?');
+alert('Awesome, ' + userColor + ' is my favorite color too!');
