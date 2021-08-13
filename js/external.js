d1b7cfb835c304d5940a39e9f0e27de828e531da
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-08-12 22:02:54 -0500

    Feat: school availability exercise

--- js/external.js
+++ js/external.js
@@ -1,15 +1,7 @@
 "use strict";
-// numbers are prompts
-// use an alert to show the results
 
-var work1 = 400;
-var work2 = 380;
-var work3 = 350;
+ var classSize = confirm("Is the class you are trying to join full?");
 
-var input1 = prompt("How many hours did you work with Google?");
-var input2 = prompt("How many hours did you work with Amazon?");
-var input3 = prompt("How many hours did you work with Facebook?");
+ var classSchedule = confirm("Do you have a class at 1700 already?");
 
-var totalPay = work1 * input1 + work2 * input2 + work3 * input3;
-
-alert("Based on the pay rate of each employer, you have earned: $" + totalPay + " so far this week.");
+ alert("You can join the class is a " + (!classSize && !classSchedule) + " statement.");
