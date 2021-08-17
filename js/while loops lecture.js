3ce0df3610ce992b4ef3ba24776793c1f26e83f2
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-08-17 13:52:07 -0500

    Feat: Mainly conditionals as well as loop lecture.

---
+++ js/while loops lecture.js
@@ -0,0 +1,88 @@
+"use strict";
+
+
+// While Loops
+
+// are we there yet?
+// var areWeThereYet = false;
+//
+// while(areWeThereYet){
+//     areWeThereYet = confirm("Are we there yet?");
+// }
+
+
+//var haveToy = confirm("Can I have the toy, please?");
+
+//Past modules: Maybe use the var into an alert to show that we've captured the value.
+//alert("Hey, it is " + haveToy + " that you can have the toy");
+
+//Now: How we can use this variable + boolean inside with a while loop?
+
+
+// while(!haveToy){
+//     haveToy = confirm("Please can I have the toy?");
+// }
+//
+// if(haveToy){
+//     alert("Thanks pops! You're the best :)");
+// }
+
+// Notice that much like an if statement, our while loop will only run if the initial condition is met.
+// If we give the Codie the toy from the get go, we won't see the behavior in the loop at all.
+
+// TODO: Quasar~ create a WHILE loop that runs as long as i is less than 10;
+// as long as the var i is less than 10. console.log information about what's going on?
+//AVOID AN INFINITE LOOP - remember you need, in some way, to add from i [think like a counter.]
+
+// var i = 0;
+// while(i <= 10){
+//     if(i++){
+//         console.log(i++);
+//     }else{
+//         console.log("The loop is done");
+//     }
+// }
+
+//While loop up to 10:
+// var i = 0;
+// while (i <= 10){
+//     console.log("Currently the variable I is : " + i);
+//     i += 1;
+// }
+// console.log("~~~~~~~~~~~~~~~~~");
+// //While loop down to 10
+// var j = 10;
+// while (j >= 0){
+//     console.log("Hey there! J is currently : " + j);
+//     j--;
+// }
+
+
+
+// ~*~ Do while loops ~*~
+
+// A do/while loop is quite similar to a while loop. The biggest difference is that a do/while loop will run at least once.
+//[do this thing for sure this first time.. then check condition to continue vs. check condition.. will I do this thing at all?]
+
+// do {
+// code to be run
+// } while (condition)
+
+// consider:
+
+// while(false){
+//     console.log("Will you ever see me?");
+// }
+//
+// do {
+//     console.log("But what about me?");
+// } while (false);
+
+// do {
+//     var secret = prompt("What is the secret password?")
+// } while(secret !== "sesame");
+
+
+
+
+
