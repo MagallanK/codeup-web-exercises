84cc7bd54b3322688c2039c5037c3bf89e848d8d
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-09-10 09:54:52 -0500

    Feat: flexbox-nav-exercise.css && html

--- js/while.js
+++ js/while.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// While loops exercise
 var i = 1;
 
 while(i < 65536){
@@ -7,3 +7,16 @@ while(i < 65536){
         console.log(i*=2);
     }
 }
+
+
+//Do While loop
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+
+do {
+    console.log("Here are the amount of cones you bought: " + Math.floor(Math.random() * 5) + 1);
+} while(allCones < 50);
+
+
+
