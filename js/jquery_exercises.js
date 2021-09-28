d5217711fde486a240e3875df41a53b993119679
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-09-28 16:08:05 -0500

    Mouse events exercise -- jquery_exercises.js

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -13,7 +13,6 @@
 // }
 
 
-
 //Example 'id selectors' 1-6 tests
 
 // var heading = $('#main-header').html();
@@ -31,15 +30,59 @@
 
 //Example for 'Element Selectors' 1-4 tests
 
-$('li').css('font-size', '20px');
-
-$('h1').css('background-color', 'blue');
-$('p').css('background-color', 'green');
-$('li').css('background-color', 'yellow');
-
-var header = $('h1').html();
-alert(header);
+// $('li').css('font-size', '20px');
+//
+// $('h1').css('background-color', 'blue');
+// $('p').css('background-color', 'green');
+// $('li').css('background-color', 'yellow');
+//
+// var header = $('h1').html();
+// alert(header);
 
 
 //Example for 'Multiple Selectors' 1 test
-$('h1, p, li').css('background-color', 'orange');
+// $('h1, p, li').css('background-color', 'orange');
+
+//-------------------------Next Lesson Below-----------------------------------------\\
+
+//Mouse events exercises 1-4 tests
+
+
+//click event for h1 tag.... Figure out how to make responsive based off user input. Commented out for now for passing results through course.
+$('h1').click(function () {
+    confirm('By clicking this title, the background color will now change. Do you want to continue?');
+    // if (confirm === true) {
+        $(this).css('background-color', 'red').css('font-family', 'helvetica');
+    // }
+    // if (confirm === false) {
+    //     alert('You chose not to change the background at this time.');
+    //     $(this).css('background-color', 'none').css('font-family', 'none');
+    // }
+});
+
+
+//dblclick event for p tag
+$('p').dblclick(function () {
+    $(this).css('font-size', '18px').css('font-family', 'cursive').css('background-color', 'yellow');
+});
+
+
+//hover event for li tags
+$('li').hover(
+    function () {
+        $(this).css('background-color', 'green').css('font-size', '22px');
+    },
+    function () {
+        $(this).css('background-color', 'inherit').css('font-size', 'inherit');
+    }
+);
+
+//h3 tag event
+$('h3').hover(
+    function () {
+        $(this).text('This text is above the list of items and below the h1 tag.').css('background-color', 'orange');
+    },
+    function () {
+        $(this).text('NOTICE: Hover over this text.').css('background-color', 'inherit');
+    }
+);
