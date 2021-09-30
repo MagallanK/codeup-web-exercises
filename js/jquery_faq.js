2b305191bf5ee47b1a654d1488c450a3aa02e31c
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-09-30 16:59:34 -0500

    Started next exercise of traverse to jquery.js

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,13 +1,6 @@
-// 'use strict';
-//
-//     $('#make-visible').click(function(){
-//         $('.invisible').addClass('visible');
-// });
-
-
 'use strict';
 
-//Pressing link shows hidden text elements for all 'titles'
+//Pressing button shows hidden text elements for all 'titles'
 $('#make-visible').click(function(e){
     e.preventDefault();
     $('.invisible').toggleClass('visible');
@@ -16,4 +9,10 @@ $('#make-visible').click(function(e){
 //Clicking each 'title' will now highlight the toggled 'title'
 $('dt').click(function(){
     $(this).toggleClass('highlight');
+});
+
+//Pressing 2nd button will highlight all last items in ul
+$('#facts-btn').click(function(e){
+    e.preventDefault();
+    $('li').last().css('background-color', 'yellow');
 });
