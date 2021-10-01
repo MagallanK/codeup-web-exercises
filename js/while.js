7e31adc4f9a40924f025fe6e6662596821f84c43
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-10-01 15:46:26 -0500

    ajax-lecture-notes.html && .js. Added while.js to complete that exercise

--- js/while.js
+++ js/while.js
@@ -2,21 +2,25 @@
 // While loops exercise
 var i = 1;
 
-while(i < 65536){
-    if(i * 2){
-        console.log(i*=2);
+while (i < 65536) {
+    if (i * 2) {
+        console.log(i *= 2);
     }
 }
 
 
 //Do While loop
 
-var allCones = Math.floor(Math.random() * 50) + 50;
+// This is how you get a random number between 50 and 100
+var totalCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+var boughtCones = Math.floor(Math.random() * 5) + 1;
 
+console.log("There are " + totalCones + " cones total.");
 
 do {
-    console.log("Here are the amount of cones you bought: " + Math.floor(Math.random() * 5) + 1);
-} while(allCones < 50);
+    var leftOverCones = totalCones - boughtCones;
+    console.log("There are " + leftOverCones + " cones left over.");
+} while (totalCones <= 0);
 
-
-
+console.log("Here are the amount of cones you bought: " + boughtCones);
