186a4b8707bac3f9caae3bfb3f59747b6073d281
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-10-03 19:13:04 -0500

    jquery.html completed.

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//toggleClass is being used only so it can be users choice of on or off.
+
 //Pressing button shows hidden text elements for all 'titles'
 $('#make-visible').click(function(e){
     e.preventDefault();
@@ -14,5 +16,17 @@ $('dt').click(function(){
 //Pressing 2nd button will highlight all last items in ul
 $('#facts-btn').click(function(e){
     e.preventDefault();
-    $('li').last().css('background-color', 'yellow');
+    $('ul li:nth-last-child(1)').toggleClass('list-highlight');
+});
+
+// Clicking titles for bottom ul will now bold each li text
+$('h3').click(function(e){
+    e.preventDefault();
+    $('li').toggleClass('title-text')
+});
+
+// Clicking on any li will now change font color to 'deepskyblue'
+$('li').click(function(e){
+    e.preventDefault();
+    $('ul li:nth-of-type(1)').toggleClass('text-color');
 });
