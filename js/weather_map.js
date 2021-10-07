e4bf61c170052258fdf56d7b2817da5f73d2cabe
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-10-07 13:50:28 -0500

    Added weather_map.css for styling

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,11 +1,22 @@
 "use strict";
 
+// added map to get display on website of map
+mapboxgl.accessToken = mapboxApiKey;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/satellite-streets-v11',
+    zoom: 10,
+    center: [-98.4916, 29.4252],
+    dragRotate: false,
+});
 
-
-$.get("http://api.openweathermap.org/data/2.5/forecast", {
+// open weather map layout for 5 days forecast
+$.get("https://api.openweathermap.org/data/2.5/onecall", {
     APPID: mapboxWeatherKey,
-    q:     "San Antonio, US",
+    lat: 29.4252,
+    lon: -98.4916,
     units: 'imperial',
+    exclude: 'minutely, hourly, alerts'
 }).done(function(data) {
     console.log(data);
 });
