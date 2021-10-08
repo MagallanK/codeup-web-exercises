7341f5e476cde2ae7ae8d18b11efa2eb6a1bc3b3
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-10-08 11:40:41 -0500

    Logged one day so far. Need to display next onto page

--- js/weather_map.js
+++ js/weather_map.js
@@ -11,6 +11,9 @@ var map = new mapboxgl.Map({
 });
 
 // open weather map layout for 5 day forecast
+
+var dailyData = [];
+
 $.get("https://api.openweathermap.org/data/2.5/onecall", {
     APPID: mapboxWeatherKey,
     lat: 29.4241,
@@ -19,20 +22,58 @@ $.get("https://api.openweathermap.org/data/2.5/onecall", {
     exclude: 'hourly, minutely, alerts',
 }).done(function(data) {
     console.log(data);
+    dailyData = data.daily;
 });
 
 
 //Work on getting one weather date to display.
 //Array of days of the week, want to loop through the array and append each to the html buttons for weather info
-var daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-$(".monday").click(function(e){
-    e.preventDefault();
-    $.get("https://api.openweathermap.org/data/2.5/onecall").done(function(data, status){
+// var daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
-    })
+// console.log one weather report when clicking button for monday
+$("button[data-target='#collapseMonday']").click(function(e){
+    e.preventDefault();
+    console.log(dailyData[1]); // can declare index here for specific days of the week [#]
 })
+
+//Need to append to html to display weather in the Monday div container after clicking button.
+
+$(document).ready(function(){
+    $.get("https://api.openweathermap.org/data/2.5/onecall", function(data){
+        var html = "";
+        $.each(data, function(index){
+            html += "<div>";
+            html += "<p>" + data[index].day + "</p>";
+            html += "</div>";
+        });
+        $("#collapseMonday").append(html);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function for converting the date to standard notation
 // function parseDate(timestamp) {
 //     return new Date(timestamp * 1000).toLocaleDateString();
+// }
+// function convertDateTime(time){
+//
+//     var date = new Date(time * 1000);
+//     console.log(date.toLocaleDateString("en-US"));
 // }
